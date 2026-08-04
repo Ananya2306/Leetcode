7009ded6d7153af5b5346becde2b82b33fec8220
~ Ananya ~ <ananya.arora.cs28@iilm.edu>
2026-08-04 16:18:18 +0530

    Time: 1 ms (93.3%), Space: 42.9 MB (14.18%) - LeetHub

---
+++ 0029-divide-two-integers/0029-divide-two-integers.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if (dividend == Integer.MIN_VALUE && divisor == -1) 
+            return Integer.MAX_VALUE;
+        boolean neg = (dividend < 0) ^ (divisor < 0);
+        long a = Math.abs((long) dividend);
+        long b = Math.abs((long) divisor);
+        long ans = 0;
+        for (int i = 31; i >= 0; i--) {
+            if ((b << i) <= a) { 
+                a -= (b << i); 
+                ans |= (1L << i); 
+                }
+        }
+        return (int) (neg ? -ans : ans);
+    }
+}
